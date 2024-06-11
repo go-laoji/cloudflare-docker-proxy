e3194254473990474d9a60ee4bc93e3d6b3c557c
go-laoji <92168729+go-laoji@users.noreply.github.com>
2024-06-11 12:01:24 +0800

    change domain

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.bitdata.io": "https://registry-1.docker.io",
+  "quay.bitdata.io": "https://quay.io",
+  "gcr.bitdata.io": "https://gcr.io",
+  "k8s-gcr.bitdata.io": "https://k8s.gcr.io",
+  "k8s.bitdata.io": "https://registry.k8s.io",
+  "ghcr.bitdata.io": "https://ghcr.io",
+  "cloudsmith.bitdata.io": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
