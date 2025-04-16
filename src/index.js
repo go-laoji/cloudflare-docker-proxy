a800ea34655ea180a4538111818981f91bbdd554
go-laoji <92168729+go-laoji@users.noreply.github.com>
2025-04-16 13:56:58 +0800

    add console log

--- src/index.js
+++ src/index.js
@@ -106,6 +106,7 @@ async function handleRequest(request) {
   }
   // foward requests
   const newUrl = new URL(upstream + url.pathname);
+  console.log(request.headers);
   const newReq = new Request(newUrl, {
     method: request.method,
     headers: request.headers,
